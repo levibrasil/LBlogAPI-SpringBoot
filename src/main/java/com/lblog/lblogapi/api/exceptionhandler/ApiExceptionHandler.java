e6a01db9482247fd941b586d7975cc2ac203f013
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-18 03:40:39 -0300

    Adicionando Exception Handler para e-mail já cadastrado

--- src/main/java/com/lblog/lblogapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/lblog/lblogapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.lblog.lblogapi.api.exceptionhandler;
 
+import com.lblog.lblogapi.domain.exception.NegocioException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,6 +11,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -43,4 +45,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, problema, headers, status, request);
     }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Problema problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setDataHora(LocalDateTime.now());
+        problema.setTitulo(ex.getMessage());
+
+        return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+    }
+
 }
