4c025de27b0a60e2576e739963b30e9c855ebe62
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-18 01:09:23 -0300

    Alterando a consulta JPQL para a utilização do ClienteRepository

--- src/main/java/com/lblog/lblogapi/controller/ClienteController.java
+++ src/main/java/com/lblog/lblogapi/controller/ClienteController.java
@@ -1,24 +1,23 @@
 package com.lblog.lblogapi.controller;
 
 import com.lblog.lblogapi.domain.model.Cliente;
+import com.lblog.lblogapi.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
 
-        return entityManager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+        return clienteRepository.findAll();
     }
 
 }
