7a0ccf2b711ad78667f8e6733a5a11b29f21c451
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-18 03:37:49 -0300

    Implementando Domain Service

---
+++ src/main/java/com/lblog/lblogapi/domain/service/CatalogoClienteService.java
@@ -0,0 +1,34 @@
+package com.lblog.lblogapi.domain.service;
+
+import com.lblog.lblogapi.domain.exception.NegocioException;
+import com.lblog.lblogapi.domain.model.Cliente;
+import com.lblog.lblogapi.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class CatalogoClienteService {
+
+    private ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente salvar(Cliente cliente) {
+        boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail())
+                .stream()
+                .anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
+
+        if (emailEmUso) {
+            throw new NegocioException("Já existe um cliente cadastrado com este e-mail");
+        }
+
+        return clienteRepository.save(cliente);
+    }
+
+    @Transactional
+    public void excluir(Long clienteId) {
+        clienteRepository.deleteById(clienteId);
+    }
+
+}
