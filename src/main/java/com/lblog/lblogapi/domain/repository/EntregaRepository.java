e2cde45fb4a9041f721d9739d9f37bd2cfb06589
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-19 00:38:49 -0300

    Criando repository Entrega

---
+++ src/main/java/com/lblog/lblogapi/domain/repository/EntregaRepository.java
@@ -0,0 +1,8 @@
+package com.lblog.lblogapi.domain.repository;
+
+import com.lblog.lblogapi.domain.model.Entrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+
+}
