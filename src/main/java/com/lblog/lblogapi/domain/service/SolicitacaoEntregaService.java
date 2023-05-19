d2671f253551333067f2b3e6d4e63391b1a79d1c
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-19 00:44:03 -0300

    Implementando o service para entrega

---
+++ src/main/java/com/lblog/lblogapi/domain/service/SolicitacaoEntregaService.java
@@ -0,0 +1,34 @@
+package com.lblog.lblogapi.domain.service;
+
+import com.lblog.lblogapi.domain.exception.NegocioException;
+import com.lblog.lblogapi.domain.model.Cliente;
+import com.lblog.lblogapi.domain.model.Entrega;
+import com.lblog.lblogapi.domain.model.StatusEntrega;
+import com.lblog.lblogapi.domain.repository.ClienteRepository;
+import com.lblog.lblogapi.domain.repository.EntregaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Service
+public class SolicitacaoEntregaService {
+
+    private CatalogoClienteService catalogoClienteService;
+
+    private EntregaRepository entregaRepository;
+
+    @Transactional
+    public Entrega solicitar(Entrega entrega) {
+        Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+
+        entrega.setCliente(cliente);
+        entrega.setStatus(StatusEntrega.PENDENTE);
+        entrega.setDataPedido(LocalDateTime.now());
+
+        return entregaRepository.save(entrega);
+    }
+
+}
