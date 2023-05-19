1e1a8add2b8c01ffa00c0071b271f7347c49577f
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-19 02:44:44 -0300

    Implementando a classe DTO de cliente resumido

---
+++ src/main/java/com/lblog/lblogapi/api/dto/ClienteResumoDTO.java
@@ -0,0 +1,13 @@
+package com.lblog.lblogapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClienteResumoDTO {
+
+    private Long id;
+    private String nome;
+
+}
