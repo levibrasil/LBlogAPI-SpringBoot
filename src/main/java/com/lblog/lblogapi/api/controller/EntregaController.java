7b6e1757bc89ae5cf2fb0a0d97a2e655b4e2ad3e
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-19 02:49:56 -0300

    Alterando os tipos de inputs e retornos para os DTOs

--- src/main/java/com/lblog/lblogapi/api/controller/EntregaController.java
+++ src/main/java/com/lblog/lblogapi/api/controller/EntregaController.java
@@ -2,6 +2,8 @@ package com.lblog.lblogapi.api.controller;
 
 import com.lblog.lblogapi.api.dto.DestinatarioDTO;
 import com.lblog.lblogapi.api.dto.EntregaDTO;
+import com.lblog.lblogapi.api.dto.input.EntregaInput;
+import com.lblog.lblogapi.assembler.EntregaAssembler;
 import com.lblog.lblogapi.domain.model.Entrega;
 import com.lblog.lblogapi.domain.repository.EntregaRepository;
 import com.lblog.lblogapi.domain.service.SolicitacaoEntregaService;
@@ -22,40 +24,29 @@ public class EntregaController {
 
     private SolicitacaoEntregaService solicitacaoEntregaService;
 
+    private EntregaAssembler entregaAssembler;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
+    public EntregaDTO solicitar(@Valid @RequestBody EntregaInput entregaInput) {
+        Entrega novaEntrega = entregaAssembler.toEntity(entregaInput);
 
-        return solicitacaoEntregaService.solicitar(entrega);
+        Entrega entregaSolicitada = solicitacaoEntregaService.solicitar(novaEntrega);
+
+        return entregaAssembler.toDTO(entregaSolicitada);
     }
 
     @GetMapping
-    public List<Entrega> listar() {
+    public List<EntregaDTO> listar() {
 
-        return entregaRepository.findAll();
+        return entregaAssembler.toColectionDTO(entregaRepository.findAll());
     }
 
     @GetMapping("/{entregaId}")
     public ResponseEntity<EntregaDTO> buscar(@PathVariable Long entregaId) {
 
         return entregaRepository.findById(entregaId)
-                .map(entrega -> {
-                    EntregaDTO dto = new EntregaDTO();
-                    dto.setId(entrega.getId());
-                    dto.setNomeCliente(entrega.getCliente().getNome());
-                    dto.setDestinatario(new DestinatarioDTO());
-                    dto.getDestinatario().setNome(entrega.getDestinatario().getNome());
-                    dto.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-                    dto.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-                    dto.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-                    dto.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-                    dto.setTaxa(entrega.getTaxa());
-                    dto.setStatus(entrega.getStatus());
-                    dto.setDataPedido(entrega.getDataPedido());
-                    dto.setDataFinalizacao(entrega.getDataFinalizacao());
-
-                    return ResponseEntity.ok().body(dto);
-                })
+                .map(entrega -> ResponseEntity.ok().body(entregaAssembler.toDTO(entrega)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
