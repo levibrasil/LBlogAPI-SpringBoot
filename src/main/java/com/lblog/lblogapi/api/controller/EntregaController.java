f6837d4ad9d6671ba00ec515ea16f8b5acbf2136
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-19 01:17:37 -0300

    Adicionando validadores em cascata para uma entrega

--- src/main/java/com/lblog/lblogapi/api/controller/EntregaController.java
+++ src/main/java/com/lblog/lblogapi/api/controller/EntregaController.java
@@ -1,23 +1,44 @@
 package com.lblog.lblogapi.api.controller;
 
 import com.lblog.lblogapi.domain.model.Entrega;
+import com.lblog.lblogapi.domain.repository.EntregaRepository;
 import com.lblog.lblogapi.domain.service.SolicitacaoEntregaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/entregas")
 public class EntregaController {
 
+    private EntregaRepository entregaRepository;
+
     private SolicitacaoEntregaService solicitacaoEntregaService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Entrega solicitar(@RequestBody Entrega entrega) {
+    public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 
         return solicitacaoEntregaService.solicitar(entrega);
+    }
+
+    @GetMapping
+    public List<Entrega> listar() {
+
+        return entregaRepository.findAll();
+    }
+
+    @GetMapping("/{entregaId}")
+    public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+
+        return entregaRepository.findById(entregaId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
