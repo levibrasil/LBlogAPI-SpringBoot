901b67c051c815795fe9cb4bcbc7fc6d739904e7
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-19 01:48:33 -0300

    Adicionando DTO para Entrega e Destinatário

---
+++ src/main/java/com/lblog/lblogapi/api/dto/EntregaDTO.java
@@ -0,0 +1,22 @@
+package com.lblog.lblogapi.api.dto;
+
+import com.lblog.lblogapi.domain.model.StatusEntrega;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class EntregaDTO {
+
+    private Long id;
+    private String nomeCliente;
+    private DestinatarioDTO destinatario;
+    private BigDecimal taxa;
+    private StatusEntrega status;
+    private OffsetDateTime dataPedido;
+    private OffsetDateTime dataFinalizacao;
+
+}
