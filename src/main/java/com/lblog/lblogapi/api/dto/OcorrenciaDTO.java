5cc92187c9f64db45a0b57fd6c5b37892ed5c70a
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-22 23:48:41 -0300

    Implementando DTO para ocorrencia

---
+++ src/main/java/com/lblog/lblogapi/api/dto/OcorrenciaDTO.java
@@ -0,0 +1,16 @@
+package com.lblog.lblogapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class OcorrenciaDTO {
+
+    private Long id;
+    private String descricao;
+    private OffsetDateTime dataRegistro;
+
+}
