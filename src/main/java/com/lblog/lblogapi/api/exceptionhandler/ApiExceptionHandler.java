0c5cba6909d70e8973cd48384faf97b0a3c12c11
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-23 00:20:16 -0300

    Adicionando exception para a entidade não encontrada

--- src/main/java/com/lblog/lblogapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/lblog/lblogapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.lblog.lblogapi.api.exceptionhandler;
 
+import com.lblog.lblogapi.domain.exception.EntidadeNaoEncontradaException;
 import com.lblog.lblogapi.domain.exception.NegocioException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
@@ -49,6 +50,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Problema problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setDataHora(OffsetDateTime.now());
+        problema.setTitulo(ex.getMessage());
+
+        return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<Object> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
 
         Problema problema = new Problema();
         problema.setStatus(status.value());
