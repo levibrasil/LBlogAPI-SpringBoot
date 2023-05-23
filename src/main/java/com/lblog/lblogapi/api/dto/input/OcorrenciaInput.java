3e45a08eb320dd37284d73800eebf722369ecf1c
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-22 23:49:00 -0300

    Implementando Input para ocorrencia

---
+++ src/main/java/com/lblog/lblogapi/api/dto/input/OcorrenciaInput.java
@@ -0,0 +1,15 @@
+package com.lblog.lblogapi.api.dto.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class OcorrenciaInput {
+
+    @NotBlank
+    private String descricao;
+
+}
