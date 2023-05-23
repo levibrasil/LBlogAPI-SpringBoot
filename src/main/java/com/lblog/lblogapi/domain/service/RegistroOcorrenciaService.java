6eb9675002840bec149e56a3a050062f364faf9d
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-22 23:46:12 -0300

    Implementando service para adicionar uma ocorrência

---
+++ src/main/java/com/lblog/lblogapi/domain/service/RegistroOcorrenciaService.java
@@ -0,0 +1,22 @@
+package com.lblog.lblogapi.domain.service;
+
+import com.lblog.lblogapi.domain.model.Entrega;
+import com.lblog.lblogapi.domain.model.Ocorrencia;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class RegistroOcorrenciaService {
+
+    private BuscaEntregaService buscaEntregaService;
+
+    @Transactional
+    public Ocorrencia registrar(Long entregaId, String descricao) {
+        Entrega entrega = buscaEntregaService.buscar(entregaId);
+
+        return entrega.adicionarOcorrencia(descricao);
+    }
+
+}
