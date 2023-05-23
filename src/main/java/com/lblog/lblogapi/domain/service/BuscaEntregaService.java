28cd6ad5bdfeb0a089f51eced98b18d400d3d27a
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-22 23:45:35 -0300

    Implementando service para buscar entrega pelo ID

---
+++ src/main/java/com/lblog/lblogapi/domain/service/BuscaEntregaService.java
@@ -0,0 +1,20 @@
+package com.lblog.lblogapi.domain.service;
+
+import com.lblog.lblogapi.domain.exception.NegocioException;
+import com.lblog.lblogapi.domain.model.Entrega;
+import com.lblog.lblogapi.domain.repository.EntregaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class BuscaEntregaService {
+
+    private EntregaRepository entregaRepository;
+
+    public Entrega buscar(Long entregaId) {
+        return entregaRepository.findById(entregaId)
+                .orElseThrow(() -> new NegocioException("Entrega não encontrada"));
+    }
+
+}
