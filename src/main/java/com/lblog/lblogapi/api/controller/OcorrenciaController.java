6c948aa5bd4cdac3580f1ffc59ab05a08edf0792
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-23 00:31:00 -0300

    Adicionando método get para listar ocorrências de uma entrega

--- src/main/java/com/lblog/lblogapi/api/controller/OcorrenciaController.java
+++ src/main/java/com/lblog/lblogapi/api/controller/OcorrenciaController.java
@@ -3,18 +3,23 @@ package com.lblog.lblogapi.api.controller;
 import com.lblog.lblogapi.api.assembler.OcorrenciaAssembler;
 import com.lblog.lblogapi.api.dto.OcorrenciaDTO;
 import com.lblog.lblogapi.api.dto.input.OcorrenciaInput;
+import com.lblog.lblogapi.domain.model.Entrega;
 import com.lblog.lblogapi.domain.model.Ocorrencia;
+import com.lblog.lblogapi.domain.service.BuscaEntregaService;
 import com.lblog.lblogapi.domain.service.RegistroOcorrenciaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/entregas/{entregaId}/ocorrencias")
 public class OcorrenciaController {
+
+    private BuscaEntregaService buscaEntregaService;
 
     private RegistroOcorrenciaService registroOcorrenciaService;
 
@@ -27,6 +32,13 @@ public class OcorrenciaController {
                 .registrar(entregaId, ocorrenciaInput.getDescricao());
 
         return ocorrenciaAssembler.toDTO(ocorrencia);
+    }
+
+    @GetMapping
+    public List<OcorrenciaDTO> listar(@PathVariable Long entregaId) {
+        Entrega entrega = buscaEntregaService.buscar(entregaId);
+
+        return ocorrenciaAssembler.toCollectionDTO(entrega.getOcorrencias());
     }
 
 }
